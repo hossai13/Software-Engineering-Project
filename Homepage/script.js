128954659f4b063ed688e364b9db17d9442aaab5
Md Hossain <hossai13@go.stockton.edu>
2024-10-07 15:07:16 -0400

    1st rendition of homepage

---
+++ Homepage/script.js
@@ -0,0 +1,40 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById('lottie-cart'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '/Homepage/Cart.json'
+  });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const title = document.querySelector('.title');
+  const slideshow = document.querySelector('.slideshow-container');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate');
+      }
+    });
+  });
+
+  observer.observe(title);
+  observer.observe(slideshow-container); 
+});
+
+let slideIndex = 0;
+slideShow();
+
+function slideShow() {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(slideShow, 6000);
+
+}
+f
